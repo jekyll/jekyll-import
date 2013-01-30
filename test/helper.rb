6cc0a5c8622ff57c4a56cd11918e0b1ec895a8bf
Parker Moore <parkrmoore@gmail.com>
2013-01-31 00:00:48 +0100

    Add tests. Thanks, @mattr-. :heart:

---
+++ test/helper.rb
@@ -0,0 +1,39 @@
+if RUBY_VERSION > '1.9' && ENV["COVERAGE"] == "true"
+  require 'simplecov'
+  require 'simplecov-gem-adapter'
+  SimpleCov.start('gem')
+end
+
+require 'test/unit'
+require 'redgreen' if RUBY_VERSION < '1.9'
+require 'shoulda'
+require 'rr'
+
+# Send STDERR into the void to suppress program output messages
+STDERR.reopen(test(?e, '/dev/null') ? '/dev/null' : 'NUL:')
+
+class Test::Unit::TestCase
+  include RR::Adapters::TestUnit
+
+  def dest_dir(*subdirs)
+    File.join(File.dirname(__FILE__), 'dest', *subdirs)
+  end
+
+  def source_dir(*subdirs)
+    File.join(File.dirname(__FILE__), 'source', *subdirs)
+  end
+
+  def clear_dest
+    FileUtils.rm_rf(dest_dir)
+  end
+
+  def capture_stdout
+    $old_stdout = $stdout
+    $stdout = StringIO.new
+    yield
+    $stdout.rewind
+    return $stdout.string
+  ensure
+    $stdout = $old_stdout
+  end
+end
---
+++ test/test_wordpress_migrator.rb
@@ -0,0 +1,12 @@
+$:.unshift File.expand_path("../lib", File.dirname(__FILE__)) # load from jekyll-import/lib
+
+require 'helper'
+require 'jekyll/migrators/wordpress'
+require 'htmlentities'
+
+class TestWordpressMigrator < Test::Unit::TestCase
+  should "clean slashes from slugs" do
+    test_title = "blogs part 1/2"
+    assert_equal("blogs-part-1-2", Jekyll::WordPress.sluggify(test_title))
+  end
+end

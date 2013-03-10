c7017dbad133647f442f6f059474561c35a115ef
Parker Moore <parkrmoore@gmail.com>
2013-03-10 19:27:35 +0100

    Jekyll module ~> JekyllImport module

--- test/test_wordpressdotcom_migrator.rb
+++ test/test_wordpressdotcom_migrator.rb
@@ -1,9 +1,8 @@
 require 'helper'
-require 'jekyll/importers/wordpressdotcom'
 
 class TestWordpressDotComMigrator < Test::Unit::TestCase
   should "clean slashes from slugs" do
     test_title = "blogs part 1/2"
-    assert_equal("blogs-part-1-2", Jekyll::WordpressDotCom.sluggify(test_title))
+    assert_equal("blogs-part-1-2", JekyllImport::WordpressDotCom.sluggify(test_title))
   end
 end

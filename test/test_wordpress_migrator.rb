b1853669e92c1571cc0bf91fae0eb8fd9f1e469a
Parker Moore <parkrmoore@gmail.com>
2013-03-10 19:27:53 +0100

    Jekyll ~> JekyllImport in Wordpress importer test

--- test/test_wordpress_migrator.rb
+++ test/test_wordpress_migrator.rb
@@ -1,12 +1,9 @@
-$:.unshift File.expand_path("../lib", File.dirname(__FILE__)) # load from jekyll-import/lib
-
 require 'helper'
-require 'jekyll/importers/wordpress'
 require 'htmlentities'
 
 class TestWordpressMigrator < Test::Unit::TestCase
   should "clean slashes from slugs" do
     test_title = "blogs part 1/2"
-    assert_equal("blogs-part-1-2", Jekyll::WordPress.sluggify(test_title))
+    assert_equal("blogs-part-1-2", JekyllImport::WordPress.sluggify(test_title))
   end
 end

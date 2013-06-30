5ff3817525daa580a28c9566b8a457acdd888fea
Parker Moore <parkrmoore@gmail.com>
2013-07-01 01:17:09 +0200

    Fixes for the CSV importer.

--- lib/jekyll/jekyll-import/csv.rb
+++ lib/jekyll/jekyll-import/csv.rb
@@ -1,11 +1,15 @@
+require "csv"
+
 module JekyllImport
   module CSV
     # Reads a csv with title, permalink, body, published_at, and filter.
     # It creates a post file for each row in the csv
-    def self.process(file = "posts.csv")
+    def self.process(options)
+      file = options[:file] || "posts.csv"
       FileUtils.mkdir_p "_posts"
       posts = 0
-      FasterCSV.foreach(file) do |row|
+      abort "Cannot find the file '#{file}'. Aborting." unless File.file?(file)
+      ::CSV.foreach(file) do |row|
         next if row[0] == "title"
         posts += 1
         name = row[3].split(" ")[0]+"-"+row[1]+(row[4] =~ /markdown/ ? ".markdown" : ".textile")

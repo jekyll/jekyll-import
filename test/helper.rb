591b18415c563bc18e04376d429b3b302fa53b1b
Parker Moore <parkrmoore@gmail.com>
2013-07-01 00:26:21 +0200

    I hate active support.

--- test/helper.rb
+++ test/helper.rb
@@ -10,7 +10,7 @@ require 'shoulda'
 require 'rr'
 
 unless defined?(Test::Unit::AssertionFailedError)
-  require 'activesupport'
+  require 'active_support'
   class Test::Unit::AssertionFailedError < ActiveSupport::TestCase::Assertion
   end
 end

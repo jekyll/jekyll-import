9bb17cf7ed2e2ee5a1022ed0684ea083437a44f2
Aniket Pant <me@aniketpant.com>
2013-08-13 10:26:04 +0530

    Decouple code and add docs
    Signed-off-by: Aniket Pant <me@aniketpant.com>

--- lib/jekyll/jekyll-import/jrnl.rb
+++ lib/jekyll/jekyll-import/jrnl.rb
@@ -26,26 +26,69 @@ module JekyllImport
       entries = input.split("\n\n");
 
       entries.each do |entry|
+        # split dateline and body
         content = entry.split("\n")
+
+        # strip dateline from jrnl entry
         dateline = content[0]
+
+        # strip body from jrnl entry
         body = content[1]
+
+        # strip timestamp from the dateline
         date = Time.parse(content[0, date_length-1].to_s)
+
+        # strip title from the dateline
         title = dateline[date_length+1, dateline.length]
+
+        # generate slug
         slug = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
 
-        data = {
-          'layout'        => layout.to_s,
-          'title'         => title.to_s,
-          'date'          => date.strftime(time_format).to_s
-        }.to_yaml
-
+        # generate filename
         filename = date.strftime("%Y-%m-%d").to_s + "-#{slug}.#{extension}"
 
-        File.open("_posts/#{filename}", "w") do |f|
-          f.puts data
-          f.puts "---"
-          f.puts body
-        end
+        meta = prepare_data(layout, title, date) # prepare YAML meta data
+        write_file(filename, meta, body) # write to file
+      end
+    end
+
+    # Prepare YAML meta data
+    #
+    # layout  - name of the layout
+    # title   - title of the entry
+    # date    - date of entry creation
+    #
+    # Examples
+    #
+    #   prepare_data("post", "Entry 1", "2013-01-01 13:00")
+    #   # => "---\nlayout: post\ntitle: Entry 1\ndate: 2013-01-01 13:00\n"
+    #
+    # Returns array converted to YAML
+    def self.prepare_data(layout, title, date)
+      data = {
+        'layout'        => layout.to_s,
+        'title'         => title.to_s,
+        'date'          => date.strftime("%Y-%m-%d %H:%M").to_s
+      }.to_yaml
+      return data;
+    end
+
+    # Writes given data to file
+    #
+    # filename    - name of the output file
+    # meta        - YAML header data
+    # body        - jrnl entry content
+    #
+    # Examples
+    #
+    #   write_file("2013-01-01-entry-1.md", "---\nlayout: post\ntitle: Entry 1\ndate: 2013-01-01 13:00\n", "This is the first entry for my new journl")
+    #
+    # Writes file to _posts/filename
+    def self.write_file(filename, meta, body)
+      File.open("_posts/#{filename}", "w") do |f|
+        f.puts meta
+        f.puts "---"
+        f.puts body
       end
     end
   end

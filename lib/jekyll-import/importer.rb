98e06c25c45b1730b5584f0bca66469a0d5d8673
Parker Moore <parkrmoore@gmail.com>
2013-11-08 17:45:03 -0500

    keep track of the subclasses of JekyllImport::Importer

--- lib/jekyll-import/importer.rb
+++ lib/jekyll-import/importer.rb
@@ -1,4 +1,13 @@
 module JekyllImport
   class Importer
+    def self.inherited(base)
+      p base
+      subclasses << base
+      subclasses.sort!
+    end
+
+    def self.subclasses
+      @subclasses ||= []
+    end
   end
 end

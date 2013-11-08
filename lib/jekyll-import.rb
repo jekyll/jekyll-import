d6b71a1e4d9dca3f2d8b1ea917bcb19015cd7227
Parker Moore <parkrmoore@gmail.com>
2013-11-08 15:03:50 -0500

    Dynamically add in the commands for Jekyll execution

--- lib/jekyll-import.rb
+++ lib/jekyll-import.rb
@@ -10,6 +10,19 @@ module JekyllImport
     @logger ||= Jekyll::Stevenson.new
   end
 
+  def self.add_importer_commands(cmd)
+    JekyllImport::Importer.subclasses.each do |importer|
+      name = importer.to_s.downcase
+      cmd.command(name.to_sym) do |c|
+        c.syntax "jekyll import #{name} [options]"
+        importer.specify_options(c)
+        c.action do |args, options|
+          importer.process(options)
+        end
+      end
+    end
+  end
+
   def self.require_with_fallback(gems)
     Array.wrap(gems).flatten.each do |gem|
       begin

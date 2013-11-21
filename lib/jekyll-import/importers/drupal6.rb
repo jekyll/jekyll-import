368a45c003bd48bb29f3d612ed06c6041b1435ad
Parker Moore <parkrmoore@gmail.com>
2013-11-20 23:03:05 -0500

    Use YAML::dump to output front-matter instead of Hash#to_s
    Should fix #90.

--- lib/jekyll-import/importers/drupal6.rb
+++ lib/jekyll-import/importers/drupal6.rb
@@ -108,7 +108,7 @@ EOF
 
           # Write out the data and content to file
           File.open("#{dir}/#{name}", "w") do |f|
-            f.puts data
+            f.puts YAML.dump(data)
             f.puts "---"
             f.puts content
           end

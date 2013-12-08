83ac5d5276fb32341b1fa39547d66673d6c9b6fa
Parker Moore <parkrmoore@gmail.com>
2013-12-07 21:19:06 -0500

    Stringify keys in the options hash

--- lib/jekyll-import/importer.rb
+++ lib/jekyll-import/importer.rb
@@ -8,7 +8,16 @@ module JekyllImport
       @subclasses ||= []
     end
 
+    def stringify_keys(hash)
+      the_hash = hash.clone
+      the_hash.keys.each do |key|
+        the_hash[(key.to_s rescue key) || key] =  the_hash.delete(key)
+      end
+      the_hash
+    end
+
     def self.run(options = {})
+      options = stringify_keys(options)
       self.require_deps
       self.validate(options) if self.respond_to?(:validate)
       self.process(options)

097d3fa90d81cc3ac13083549a623000779d87dd
Parker Moore <parkrmoore@gmail.com>
2013-12-08 01:36:40 -0500

    Restructure Behance importer such that it conforms to current setup

--- lib/jekyll-import/importers/behance.rb
+++ lib/jekyll-import/importers/behance.rb
@@ -1,72 +1,75 @@
-# Usage:
-#   ruby -r '../jekyll-import/lib/jekyll/jekyll-import/behance.rb' -e 'JekyllImport::BehanceImport.process(user: "USERNAME", api_token: "API_TOKEN")'
-
-
-require 'fileutils'
-require 'safe_yaml'
-
-require 'date'
-require 'time'
-
-begin 
-  require 'behance'
-rescue LoadError => e
-  raise unless e.message =~ /behance/
-  puts "Please install behance before using the importer!"
-end
-
 module JekyllImport
-  module BehanceImport
-    def self.validate(options)
-      abort "Missing mandatory option --user." unless options[:user]
-      abort "Missing mandatory option --api_token." unless options[:api_token]
-    end
+  module Importers
+    class Behance < Importer
+      def self.require_deps
+        JekyllImport.require_with_fallback(%w[
+          fileutils
+          safe_yaml
+          date
+          time
+          behance
+        ])
+      end
 
-    # Process the import.
-    #
-    # user - the behance user to retrieve projects (ID or username)
-    # api_token - your developer API Token
-    #
-    # Returns nothing.
-    def self.process(options)
-      validate(options)
+      def self.specify_options(c)
+        c.option 'user', '--user NAME', 'The username of the account'
+        c.option 'api_token', '--api_token TOKEN', 'The API access token for the account'
+      end
 
-      user  = options[:user]
-      token = options[:api_token]
-
-      client    = Behance::Client.new(access_token: token)
-      projects  = client.user_projects(user)
-
-      puts "#{projects.length} project(s) found. Importing now..."
-
-      projects.each do |project|
-
-        details = client.project(project['id'])
-        title   = project['name'].to_s
-        formatted_date = Time.at(project['published_on'].to_i).to_date.to_s
-
-        post_name = title.split(%r{ |!|/|:|&|-|$|,}).map do |character|
-          character.downcase unless character.empty?
-        end.compact.join('-')
-
-        name = "#{formatted_date}-#{post_name}"
-
-        header = {
-          "layout" => "post",
-          "title" => title,
-          "project" => details
-        }
-
-        FileUtils.mkdir_p("_posts")
-
-        File.open("_posts/#{name}.md", "w") do |f|
-          f.puts header.to_yaml
-          f.puts "---\n\n"
-          f.puts details['description']
+      def self.validate(options)
+        %w[user api_token].each do |option|
+          if options[option].nil?
+            abort "Missing mandatory option --#{option}."
+          end
         end
       end
 
-      puts "Finished importing."
+      # Process the import.
+      #
+      # user - the behance user to retrieve projects (ID or username)
+      # api_token - your developer API Token
+      #
+      # Returns nothing.
+      def self.process(options)
+        validate(options)
+
+        user  = options[:user]
+        token = options[:api_token]
+
+        client    = Behance::Client.new(access_token: token)
+        projects  = client.user_projects(user)
+
+        puts "#{projects.length} project(s) found. Importing now..."
+
+        projects.each do |project|
+
+          details = client.project(project['id'])
+          title   = project['name'].to_s
+          formatted_date = Time.at(project['published_on'].to_i).to_date.to_s
+
+          post_name = title.split(%r{ |!|/|:|&|-|$|,}).map do |character|
+            character.downcase unless character.empty?
+          end.compact.join('-')
+
+          name = "#{formatted_date}-#{post_name}"
+
+          header = {
+            "layout" => "post",
+            "title" => title,
+            "project" => details
+          }
+
+          FileUtils.mkdir_p("_posts")
+
+          File.open("_posts/#{name}.md", "w") do |f|
+            f.puts header.to_yaml
+            f.puts "---\n\n"
+            f.puts details['description']
+          end
+        end
+
+        puts "Finished importing."
+      end
     end
   end
 end

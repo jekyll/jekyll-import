6c9248fc7ed73a29443112981976f1adeae88bdc
cat_in_136 <cat.in.136+github@gmail.com>
2014-08-31 11:05:00 +0900

    for blogger importer, add docs and fix how-to-use message in jekyll import blogger
    Usage:
    
      jekyll import blogger [options]
    
    Options:
                --source NAME  The XML file (blog-MM-DD-YYYY.xml) path to import
                --no-blogger-info  Leave blogger-URL info (id and old URL.) as YAML data (default: false)
                --replace-internal-link  Whether to replace internal links with the post_url liquid codes (default: false)
            -h, --help         Show this message
            -v, --version      Print the name and version
            -t, --trace        Show the full backtrace when an error occurs

--- lib/jekyll-import/importers/blogger.rb
+++ lib/jekyll-import/importers/blogger.rb
@@ -5,8 +5,8 @@ module JekyllImport
     class Blogger < Importer
       def self.specify_options(c)
         c.option 'source', '--source NAME', 'The XML file (blog-MM-DD-YYYY.xml) path to import'
-        c.option 'no-blogger-info', '--no-blogger-info', 'Leave blogger-URL info (id and old URL.) as YAML data (default: false)'
-        c.option 'replace-internal-link', '--replace-internal-link', 'Whether to replace internal links with the post_url liquid codes (default: false)'
+        c.option 'no-blogger-info', '--no-blogger-info', 'not to leave blogger-URL info (id and old URL) in the front matter (default: false)'
+        c.option 'replace-internal-link', '--replace-internal-link', 'replace internal links using the post_url liquid tag. (default: false)'
       end
 
       def self.validate(options)

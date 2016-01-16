1433151a39578b51b6fba9b2baa8e3ba656090b7
Parker Moore <parkrmoore@gmail.com>
2016-01-16 15:12:24 -0800

    CSV: parse the post using a class which errors on missing data.

---
+++ test/test_csv.rb
@@ -0,0 +1,57 @@
+require 'helper'
+
+class TestCSVImporter < Test::Unit::TestCase
+  sample_row = [
+    "My Title",
+    "/2015/05/05/hi.html",
+    "Welcome to Jekyll!\n\nI am a post body.",
+    "2015-01-10",
+    "markdown"
+  ]
+
+  context "CSVPost" do
+    should "parse published_at to DateTime" do
+      post = Importers::CSV::CSVPost.new(sample_row)
+      assert post.published_at.is_a?(DateTime), "post.published_at should be a DateTime"
+      assert_equal "2015-01-10", post.published_at.strftime("%Y-%m-%d")
+    end
+
+    should "pull in metadata properly" do
+      post = Importers::CSV::CSVPost.new(sample_row)
+      assert_equal sample_row[0], post.title
+      assert_equal sample_row[1], post.permalink
+      assert_equal sample_row[2], post.body
+      assert_equal sample_row[4], post.markup
+    end
+
+    should "correctly construct source filename" do
+      post = Importers::CSV::CSVPost.new(sample_row)
+      assert_equal "2015-01-10-hi.markdown", post.filename
+    end
+  end
+
+  context "CSV importer" do
+    should "write post to proper place" do
+      FileUtils.mkdir_p "tmp/_posts"
+      Dir.chdir("tmp") do
+        post = Importers::CSV::CSVPost.new(sample_row)
+        Importers::CSV.write_post(post, {})
+        output_filename = "_posts/2015-01-10-hi.markdown"
+        assert File.exist?(output_filename), "Post should be written."
+        assert_equal File.read(output_filename), <<-CONTENTS
+---
+layout: post
+title: My Title
+date: '2015-01-10T00:00:00+00:00'
+permalink: "/2015/05/05/hi.html"
+---
+Welcome to Jekyll!
+
+I am a post body.
+CONTENTS
+        File.unlink(output_filename)
+      end
+    end
+
+  end
+end

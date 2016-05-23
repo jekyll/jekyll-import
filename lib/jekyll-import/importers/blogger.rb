0b33fa4e59bd47c5a0ccf9f1f0262ff09054fa60
Jonas Oscarsson <jonas.osc@rsson.nu>
2016-05-23 20:58:25 +0200

    Create html files for blog comments in _comments/

--- lib/jekyll-import/importers/blogger.rb
+++ lib/jekyll-import/importers/blogger.rb
@@ -143,6 +143,10 @@ module JekyllImport
             if @in_entry_elem
               @in_entry_elem[:meta][:thumbnail] = attrs['url']
             end
+          when 'thr:in-reply-to'
+            if @in_entry_elem
+              @in_entry_elem[:meta][:post_id] = attrs['ref']
+            end
           end
         end
 
@@ -182,6 +186,23 @@ module JekyllImport
               if post_data
                 target_dir = '_posts'
                 target_dir = '_drafts' if @in_entry_elem[:meta][:draft]
+
+                FileUtils.mkdir_p(target_dir)
+
+                file_name = URI::decode("#{post_data[:filename]}.html")
+                File.open(File.join(target_dir, file_name), 'w') do |f|
+                  f.flock(File::LOCK_EX)
+
+                  f << post_data[:header].to_yaml
+                  f << "---\n\n"
+                  f << post_data[:body]
+                end
+              end
+            elsif @in_entry_elem[:meta][:kind] == 'comment'
+              post_data = get_post_data_from_in_entry_elem_info
+
+              if post_data
+                target_dir = '_comments'
 
                 FileUtils.mkdir_p(target_dir)
 
@@ -235,6 +256,48 @@ module JekyllImport
               'date' => @in_entry_elem[:meta][:published],
               'author' => @in_entry_elem[:meta][:author],
               'tags' => @in_entry_elem[:meta][:category],
+            }
+            header['modified_time'] = @in_entry_elem[:meta][:updated] if @in_entry_elem[:meta][:updated] && @in_entry_elem[:meta][:updated] != @in_entry_elem[:meta][:published]
+            header['thumbnail'] = @in_entry_elem[:meta][:thumbnail] if @in_entry_elem[:meta][:thumbnail]
+            header['blogger_id'] = @in_entry_elem[:meta][:id] if @leave_blogger_info
+            header['blogger_orig_url'] = @in_entry_elem[:meta][:original_url] if @leave_blogger_info && @in_entry_elem[:meta][:original_url]
+
+            body = @in_entry_elem[:body]
+
+            # body escaping associated with liquid
+            if body =~ /{{/
+              body.gsub!(/{{/, '{{ "{{" }}')
+            end
+            if body =~ /{%/
+              body.gsub!(/{%/, '{{ "{%" }}')
+            end
+
+            { :filename => filename, :header => header, :body => body }
+          elsif @in_entry_elem[:meta][:kind] == 'comment'
+            timestamp = Time.parse(@in_entry_elem[:meta][:published]).strftime('%Y-%m-%d')
+            if @in_entry_elem[:meta][:original_url]
+              if not @comment_seq
+                @comment_seq = 1
+              end
+
+              original_uri = URI.parse(@in_entry_elem[:meta][:original_url])
+              original_path = original_uri.path.to_s
+              filename = "%s-%s-%s" %
+                [timestamp,
+                 File.basename(original_path, File.extname(original_path)),
+                 @comment_seq]
+
+              @comment_seq = @comment_seq + 1
+
+              @original_url_base = "#{original_uri.scheme}://#{original_uri.host}"
+            else
+              raise 'Original URL is missing'
+            end
+
+            header = {
+              'date' => @in_entry_elem[:meta][:published],
+              'author' => @in_entry_elem[:meta][:author],
+              'blogger_post_id' => @in_entry_elem[:meta][:post_id],
             }
             header['modified_time'] = @in_entry_elem[:meta][:updated] if @in_entry_elem[:meta][:updated] && @in_entry_elem[:meta][:updated] != @in_entry_elem[:meta][:published]
             header['thumbnail'] = @in_entry_elem[:meta][:thumbnail] if @in_entry_elem[:meta][:thumbnail]

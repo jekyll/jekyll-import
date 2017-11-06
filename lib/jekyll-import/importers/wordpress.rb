c59d26e3a95183ca9134c129aae9920340126a8b
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2017-11-06 02:48:20 +0100

    fix variable name for more_anchor (#305)
    Merge pull request 305

--- lib/jekyll-import/importers/wordpress.rb
+++ lib/jekyll-import/importers/wordpress.rb
@@ -206,7 +206,7 @@ module JekyllImport
             excerpt = content[0...more_index]
           end
           if options[:more_anchor]
-            more_link = "more"
+            more_anchor = "more"
             content.sub!(%r/<!-- *more *-->/,
                          "<a id=\"more\"></a>" \
                          "<a id=\"more-#{post[:id]}\"></a>")

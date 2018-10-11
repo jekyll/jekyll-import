765b7875265f7b29ee1224575c756a998ccb1555
Ashwin Maroli <ashmaroli@gmail.com>
2018-10-11 19:05:05 +0530

    Remove cyclic require of import command

--- lib/jekyll/commands/import.rb
+++ lib/jekyll/commands/import.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-# load from jekyll-import/lib
-$LOAD_PATH.unshift File.expand_path("../", __dir__)
-require "jekyll/command"
-require "jekyll-import"
-
 module Jekyll
   module Commands
     class Import < Command

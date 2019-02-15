0cd0f7f1b480c273a5dbb3755cbb20033bd9f46b
kinoppyd <WhoIsDissolvedGirl+github@gmail.com>
2019-02-16 02:56:54 +0900

    Escape backslash

--- test/test_util.rb
+++ test/test_util.rb
@@ -6,4 +6,10 @@ class TestUtil < Test::Unit::TestCase
     expected = "<p>this is a test</p>\n<p>and it works</p>\n"
     assert_equal(expected, Util.wpautop(original))
   end
+
+  should ".wpautop is escapes backslash" do
+    original = "<pre>/(?<word>\\w+) \\k<word>/</pre>"
+    expected = "<pre>/(?<word>\\w+) \\k<word>/</pre>\n"
+    assert_equal(expected, Util.wpautop(original))
+  end
 end

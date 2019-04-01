b4006c71cf6a9944aa8c1a94a7dd14a644e4cd33
scl <sylvain.claudel@4murs.fr>
2019-04-01 14:02:11 +0200

    RubyStyleGuide

--- lib/jekyll-import/importers/dotclear.rb
+++ lib/jekyll-import/importers/dotclear.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # Tested with dotClear 2.1.5
 
 module JekyllImport
@@ -28,20 +29,18 @@ module JekyllImport
       end
 
       def self.extract_headers_section(str)
-        str[1..-2].split(' ')[1].split(',')
+        str[1..-2].split(" ")[1].split(",")
       end
 
       def self.extract_data_section(str)
-        str.gsub(/^"/,'').gsub(/"$/,'').split('","')
+        str.gsub(/^"/, "").gsub(/"$/, "").split('","')
       end
 
       def self.process(opts)
         options = {
-            :datafile   => opts.fetch("datafile", ""),
-            :mediafolder => opts.fetch("mediafolder", "")
+          :datafile    => opts.fetch("datafile", ""),
+          :mediafolder => opts.fetch("mediafolder", "")
         }
-
-        puts options.inspect
 
         FileUtils.mkdir_p("_posts")
         FileUtils.mkdir_p("_drafts")
@@ -52,11 +51,11 @@ module JekyllImport
         keywords = {}
 
         File.readlines(options[:datafile]).each do |lineraw|
-          line = lineraw.strip.gsub(/\n$/,'')
+          line = lineraw.strip.gsub(/\n$/, "")
 
           next if line.empty?
 
-          if line.start_with?('[') # post | media \ meta | comment...
+          if line.start_with?("[") # post | media \ meta | comment...
             type_data = line.split(" ").first[1..-1]
             headers[type_data] = extract_headers_section(line)
             next
@@ -65,32 +64,32 @@ module JekyllImport
           elts = extract_data_section(line)
 
           if type_data == "post"
-            draft = (elts[headers[type_data].index('post_status')] != "1")
+            draft = (elts[headers[type_data].index("post_status")] != "1")
 
-            date_str = elts[headers[type_data].index('post_creadt')]
-            date_str_formatted = ((date_str.nil? || date_str.empty?) ? Date.today : Date.parse(date_str).strftime("%Y-%m-%d"))
-            title_param = elts[headers[type_data].index('post_title')].to_s.parameterize
+            date_str = elts[headers[type_data].index("post_creadt")]
+            date_str_formatted = (date_str.nil? || date_str.empty?) ? Date.today : Date.parse(date_str).strftime("%Y-%m-%d")
+            title_param = elts[headers[type_data].index("post_title")].to_s.parameterize
 
             content = elts[headers[type_data].index("post_content_xhtml")].to_s
-            content = content.gsub('\"', '"').gsub('\n', "\n").gsub('/public/', '/assets/img/')
+            content = content.gsub('\"', '"').gsub('\n', "\n").gsub("/public/", "/assets/images/")
 
-            filepath = File.join(Dir.pwd,(draft ? "_drafts" : "_posts"), "#{date_str_formatted}-#{title_param}.html")
+            filepath = File.join(Dir.pwd, (draft ? "_drafts" : "_posts"), "#{date_str_formatted}-#{title_param}.html")
 
             entire_content_file = <<~EOS
               ---
               layout: post
-              title: "#{elts[headers[type_data].index('post_title')]}"
-              date: #{elts[headers[type_data].index('post_creadt')]} +0100
+              title: "#{elts[headers[type_data].index("post_title")]}"
+              date: #{elts[headers[type_data].index("post_creadt")]} +0100
               categories: ABC
               ---
 
               #{content}
             EOS
 
-            posts_and_drafts[elts[headers[type_data].index('post_id')]] = { path: filepath, content: entire_content_file }
+            posts_and_drafts[elts[headers[type_data].index("post_id")]] = { path: filepath, content: entire_content_file }
           elsif type_data == "media"
-            elts[headers[type_data].index('media_title')]
-            mediafilepath = elts[headers[type_data].index('media_file')]
+            elts[headers[type_data].index("media_title")]
+            mediafilepath = elts[headers[type_data].index("media_file")]
 
             src_path = File.join(options[:mediafolder], mediafilepath)
             dst_path = File.join(Dir.pwd, "assets", "images", "#{mediafilepath}")
@@ -98,8 +97,8 @@ module JekyllImport
             FileUtils.mkdir_p(File.dirname(dst_path))
             FileUtils.cp(src_path, dst_path)
           elsif type_data == "meta"
-            keywords[elts[headers[type_data].index('post_id')]] ||= []
-            keywords[elts[headers[type_data].index('post_id')]] << elts[headers[type_data].index('meta_id')]
+            keywords[elts[headers[type_data].index("post_id")]] ||= []
+            keywords[elts[headers[type_data].index("post_id")]] << elts[headers[type_data].index("meta_id")]
           elsif type_data == "link"
 
           elsif type_data == "setting"
@@ -111,7 +110,7 @@ module JekyllImport
 
         # POST-process : Change media path in posts and drafts
         posts_and_drafts.each do |post_id, hsh|
-          keywords_str = keywords[post_id].to_a.join(' ')
+          keywords_str = keywords[post_id].to_a.join(" ")
           content_file = hsh[:content]
           content_file = content_file.gsub("categories: ABC","categories: #{keywords_str}")
 

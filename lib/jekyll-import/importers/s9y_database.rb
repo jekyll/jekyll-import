7b1261fac63e4ab8ecee769adac8771294b61d44
Judebert <github.10.judebert@xoxy.net>
2021-04-24 12:15:21 -0700

    s9y-db importer permalink, tag, excerpt, and semantic HTML improvements (#392)
    Merge pull request 392

--- lib/jekyll-import/importers/s9y_database.rb
+++ lib/jekyll-import/importers/s9y_database.rb
@@ -11,25 +11,31 @@ module JekyllImport
             fileutils
             safe_yaml
             unidecode
+            nokogiri
           )
         )
       end
 
       def self.specify_options(c)
-        c.option "dbname",         "--dbname DB",           "Database name (default: '')"
-        c.option "socket",         "--socket SOCKET",       "Database socket (default: '')"
-        c.option "user",           "--user USER",           "Database user name (default: '')"
-        c.option "password",       "--password PW",         "Database user's password (default: '')"
-        c.option "host",           "--host HOST",           "Database host name (default: 'localhost')"
-        c.option "port",           "--port PORT",           "Custom database port connect to (default: 3306)"
-        c.option "table_prefix",   "--table_prefix PREFIX", "Table prefix name (default: 'serendipity_')"
-        c.option "clean_entities", "--clean_entities",      "Whether to clean entities (default: true)"
-        c.option "comments",       "--comments",            "Whether to import comments (default: true)"
-        c.option "categories",     "--categories",          "Whether to import categories (default: true)"
-        c.option "tags",           "--tags",                "Whether to import tags (default: true)"
-        c.option "drafts",         "--drafts",              "Whether to export drafts as well"
-        c.option "markdown",       "--markdown",            "convert into markdown format (default: false)"
-        c.option "permalinks",     "--permalinks",          "preserve S9Y permalinks (default: false)"
+        c.option "dbname",            "--dbname DB",           "Database name (default: '')"
+        c.option "socket",            "--socket SOCKET",       "Database socket (default: '')"
+        c.option "user",              "--user USER",           "Database user name (default: '')"
+        c.option "password",          "--password PW",         "Database user's password (default: '')"
+        c.option "host",              "--host HOST",           "Database host name (default: 'localhost')"
+        c.option "port",              "--port PORT",           "Custom database port connect to (default: 3306)"
+        c.option "table_prefix",      "--table_prefix PREFIX", "Table prefix name (default: 'serendipity_')"
+        c.option "clean_entities",    "--clean_entities",      "Whether to clean entities (default: true)"
+        c.option "comments",          "--comments",            "Whether to import comments (default: true)"
+        c.option "categories",        "--categories",          "Whether to import categories (default: true)"
+        c.option "tags",              "--tags",                "Whether to import tags (default: true)"
+        c.option "drafts",            "--drafts",              "Whether to export drafts as well"
+        c.option "markdown",          "--markdown",            "convert into markdown format (default: false)"
+        c.option "permalinks",        "--permalinks",          "preserve S9Y permalinks (default: false)"
+        c.option "excerpt_separator", "--excerpt_separator",   "Demarkation for excerpts (default: '<a id=\"extended\"></a>')"
+        c.option "includeentry",      "--includeentry",        "Replace macros from the includeentry plugin (default: false)"
+        c.option "imgfig",            "--imgfig",              "Replace nested img and youtube divs with HTML figure tags (default: true)"
+        c.option "linebreak",         "--linebreak",           "Line break processing: wp, nokogiri, ignore (default: wp)"
+        c.option "relative",          "--relative",            "Convert links with this prefix to relative (default:nil)"
       end
 
       # Main migrator function. Call this to perform the migration.
@@ -56,36 +62,64 @@ module JekyllImport
       # :categories::     If true, save the post's categories in its
       #                   YAML front matter. Default: true.
       # :tags::           If true, save the post's tags in its
-      #                   YAML front matter. Default: true.
+      #                   YAML front matter, in lowercase.  Default: true.
       # :extension::      Set the post extension. Default: "html"
       # :drafts::         If true, export drafts as well
       #                   Default: true.
       # :markdown::       If true, convert the content to markdown
       #                   Default: false
       # :permalinks::     If true, save the post's original permalink in its
-      #                   YAML front matter. Default: false.
+      #                   YAML front matter. If the 'entryproperties' plugin
+      #                   was used, its permalink will become the canonical
+      #                   permalink, and any other will become redirects.
+      #                   Default: false.
+      # :excerpt_separator:: A string to use to separate the excerpt (body
+      #                      in S9Y) from the rest of the article (extended
+      #                      body in S9Y). Default: "<a id=\"extended\"></a>".
+      # :includentry::    Replace macros from the includentry plugin - these are
+      #                   the [s9y-include-entry] and [s9y-include-block] macros.
+      #                   Default: false.
+      # :imgfig::         Replace S9Y image-comment divs with an HTML figure
+      #                   div and figcaption, if applicable. Works for img and
+      #                   iframe.
+      #                   Default: true.
       #
+      # :linebreak::      When set to the default "wp", line breaks in entries
+      #                   will be processed WordPress style, by replacing double
+      #                   line breaks with HTML p tags, and remaining single
+      #                   line breaks with HTML br tags. When set to "nokogiri",
+      #                   entries will be loaded into Nokogiri and formatted as
+      #                   an XHTML fragment. When set to "ignore", line breaks
+      #                   will not be replaced at all.
+      #                   Default: wp
+      # :relative::       Replace absolute links (http://:relative:/foo)
+      #                   to relative links (/foo).
+
       def self.process(opts)
         options = {
-          :user           => opts.fetch("user", ""),
-          :pass           => opts.fetch("password", ""),
-          :host           => opts.fetch("host", "localhost"),
-          :port           => opts.fetch("port", 3306),
-          :socket         => opts.fetch("socket", nil),
-          :dbname         => opts.fetch("dbname", ""),
-          :table_prefix   => opts.fetch("table_prefix", "serendipity_"),
-          :clean_entities => opts.fetch("clean_entities", true),
-          :comments       => opts.fetch("comments", true),
-          :categories     => opts.fetch("categories", true),
-          :tags           => opts.fetch("tags", true),
-          :extension      => opts.fetch("extension", "html"),
-          :drafts         => opts.fetch("drafts", true),
-          :markdown       => opts.fetch("markdown", false),
-          :permalinks     => opts.fetch("permalinks", false),
+          :user              => opts.fetch("user", ""),
+          :pass              => opts.fetch("password", ""),
+          :host              => opts.fetch("host", "localhost"),
+          :port              => opts.fetch("port", 3306),
+          :socket            => opts.fetch("socket", nil),
+          :dbname            => opts.fetch("dbname", ""),
+          :table_prefix      => opts.fetch("table_prefix", "serendipity_"),
+          :clean_entities    => opts.fetch("clean_entities", true),
+          :comments          => opts.fetch("comments", true),
+          :categories        => opts.fetch("categories", true),
+          :tags              => opts.fetch("tags", true),
+          :extension         => opts.fetch("extension", "html"),
+          :drafts            => opts.fetch("drafts", true),
+          :markdown          => opts.fetch("markdown", false),
+          :permalinks        => opts.fetch("permalinks", false),
+          :excerpt_separator => opts.fetch("excerpt_separator", "<a id=\"extended\"></a>"),
+          :includeentry      => opts.fetch("includeentry", false),
+          :imgfig            => opts.fetch("imgfig", true),
+          :linebreak         => opts.fetch("linebreak", "wp"),
+          :relative          => opts.fetch("relative", nil),
         }
 
         options[:clean_entities] = require_if_available("htmlentities", "clean_entities") if options[:clean_entities]
-
         options[:markdown] = require_if_available("reverse_markdown", "markdown") if options[:markdown]
 
         FileUtils.mkdir_p("_posts")
@@ -120,6 +154,7 @@ module JekyllImport
 
         posts_query = "
            SELECT
+             'post'                 AS `type`,
              entries.ID             AS `id`,
              entries.isdraft        AS `isdraft`,
              entries.title          AS `title`,
@@ -154,36 +189,45 @@ module JekyllImport
         name = format("%02d-%02d-%02d-%s.%s", date.year, date.month, date.day, slug, extension)
 
         content = post[:body].to_s
-        content += "\n\n" + post[:body_extended].to_s unless post[:body_extended].to_s.empty?
+        extended_content = post[:body_extended].to_s
 
+        content += options[:excerpt_separator] + extended_content unless extended_content.nil? || extended_content.strip.empty?
+
+        content = process_includeentry(content, db, options) if options[:includeentry]
+        content = process_img_div(content) if options[:imgfig]
         content = clean_entities(content) if options[:clean_entities]
+        content = content.gsub(%r!href=(["'])http://#{options[:relative]}!, 'href=\1') if options[:relative]
 
         content = ReverseMarkdown.convert(content) if options[:markdown]
 
         categories = process_categories(db, options, post)
         comments = process_comments(db, options, post)
         tags = process_tags(db, options, post)
-        permalink = process_permalink(db, options, post)
+        all_permalinks = process_permalink(db, options, post)
+        primary_permalink = all_permalinks.shift
+        supplemental_permalinks = all_permalinks unless all_permalinks.empty?
 
         # Get the relevant fields as a hash, delete empty fields and
         # convert to YAML for the header.
         data = {
-          "layout"       => post[:type].to_s,
-          "status"       => status.to_s,
-          "published"    => status.to_s == "draft" ? nil : (status.to_s == "published"),
-          "title"        => title.to_s,
-          "author"       => {
+          "layout"            => post[:type].to_s,
+          "status"            => status.to_s,
+          "published"         => status.to_s == "draft" ? nil : (status.to_s == "published"),
+          "title"             => title.to_s,
+          "author"            => {
             "display_name" => post[:author].to_s,
             "login"        => post[:author_login].to_s,
             "email"        => post[:author_email].to_s,
           },
-          "author_login" => post[:author_login].to_s,
-          "author_email" => post[:author_email].to_s,
-          "date"         => date.to_s,
-          "permalink"    => options[:permalinks] ? permalink : nil,
-          "categories"   => options[:categories] ? categories : nil,
-          "tags"         => options[:tags] ? tags : nil,
-          "comments"     => options[:comments] ? comments : nil,
+          "author_login"      => post[:author_login].to_s,
+          "author_email"      => post[:author_email].to_s,
+          "date"              => date.to_s,
+          "permalink"         => options[:permalinks] ? primary_permalink : nil,
+          "redirect_from"     => options[:permalinks] ? supplemental_permalinks : nil,
+          "categories"        => options[:categories] ? categories : nil,
+          "tags"              => options[:tags] ? tags : nil,
+          "comments"          => options[:comments] ? comments : nil,
+          "excerpt_separator" => extended_content.empty? ? nil : options[:excerpt_separator],
         }.delete_if { |_k, v| v.nil? || v == "" }.to_yaml
 
         if post[:type] == "page"
@@ -195,11 +239,21 @@ module JekyllImport
           filename = "_posts/#{name}"
         end
 
+        content = case options[:linebreak]
+                  when "nokogiri"
+                    Nokogiri::HTML.fragment(content).to_xhtml
+                  when "ignore"
+                    content
+                  else
+                    # "wp" is the only remaining option, and the default
+                    Util.wpautop(content)
+                  end
+
         # Write out the data and content to file
         File.open(filename, "w") do |f|
           f.puts data
           f.puts "---"
-          f.puts Util.wpautop(content)
+          f.puts content
         end
       end
 
@@ -209,6 +263,150 @@ module JekyllImport
       rescue LoadError
         warn "Could not require '#{gem_name}', so the :#{option_name} option is now disabled."
         true
+      end
+
+      def self.process_includeentry(text, db, options)
+        return text unless options[:includeentry]
+
+        result = text
+
+        px = options[:table_prefix]
+
+        props  = text.scan(%r!(\[s9y-include-entry:([0-9]+):([^:]+)\])!)
+        blocks = text.scan(%r!(\[s9y-include-block:([0-9]+):?([^:]+)?\])!)
+
+        props.each do |match|
+          macro = match[0]
+          id = match[1]
+          replacement = ""
+          if match[2].start_with?("prop=")
+            prop = match[2].sub("prop=", "")
+            cquery = get_property_query(px, id, prop)
+          else
+            prop = match[2]
+            cquery = get_value_query(px, id, prop)
+          end
+          db[cquery].each do |row|
+            replacement << row[:txt]
+          end
+          result = result.sub(macro, replacement)
+        end
+
+        blocks.each do |match|
+          macro = match[0]
+          id = match[1]
+          replacement = ""
+          # match[2] *could* be 'template', but we can't run it through Smarty, so we ignore it
+          cquery = %(
+            SELECT
+              px.body AS `txt`
+            FROM
+              #{px}staticblocks AS px
+            WHERE
+              id = '#{id}'
+          )
+          db[cquery].each do |row|
+            replacement << row[:txt]
+          end
+          result = result.sub(macro, replacement)
+        end
+
+        result
+      end
+
+      def get_property_query(px, id, prop)
+        %(
+          SELECT
+            px.value AS `txt`
+          FROM
+            #{px}entryproperties AS px
+          WHERE
+            entryid = '#{id}' AND
+            property = '#{prop}'
+        )
+      end
+
+      def get_value_query(px, id, prop)
+        %(
+          SELECT
+            px.#{prop} AS `txt`
+          FROM
+            #{px}entries AS px
+          WHERE
+            entryid = '#{id}'
+        )
+      end
+
+      # Replace .serendipity_imageComment_* blocks
+      def self.process_img_div(text)
+        caption_classes = [
+          ".serendipity_imageComment_left",
+          ".serendipity_imageComment_right",
+          ".serendipity_imageComment_center",
+        ]
+
+        noko = Nokogiri::HTML.fragment(text)
+        noko.css(caption_classes.join(",")).each do |imgcaption|
+          block_attrs = get_block_attrs(imgcaption)
+
+          # Is this a thumbnail to a bigger/other image?
+          big_link = imgcaption.at_css(".serendipity_image_link")
+          big_link ||= imgcaption.at_xpath(".//a[.//img]")
+
+          # The caption (if any) may have raw HTML
+          caption_elem = imgcaption.at_css(".serendipity_imageComment_txt")
+          caption = ""
+          caption = "<figcaption>#{caption_elem.inner_html}</figcaption>" if caption_elem
+
+          image_node = imgcaption.at_css("img")
+          if image_node
+            attrs = get_media_attrs(image_node)
+            media = "<img #{attrs}/>"
+          else
+            iframe_node = imgcaption.at_css("iframe")
+            if iframe_node
+              attrs = get_media_attrs(iframe_node)
+              media = "<iframe #{attrs}'></iframe>"
+            else
+              STDERR.puts "Unrecognized media block: #{imgcaption}"
+              return text
+            end
+          end
+
+          # Wrap media in link, if any
+          if big_link
+            big = big_link.attribute("href")
+            media = "<a href='#{big}'>#{media}</a>"
+          end
+
+          # Replace HTML with clean media source, wrapped in figure
+          imgcaption.replace("<figure #{block_attrs}#{media}#{caption}</figure>")
+        end
+
+        noko.to_s
+      end
+
+      def get_media_attrs(node)
+        width = node.attribute("width")
+        width = "width='#{width}'" if width
+        height = node.attribute("height")
+        height = "height='#{height}'" if height
+        alt = node.attribute("alt")
+        alt = "alt='#{alt}'" if alt
+        src = "src='" + node.attribute("src") + "'"
+        [src, width, height, alt].join(" ")
+      end
+
+      def get_block_attrs(imgcaption)
+        # Extract block-level attributes
+        float = imgcaption.attribute("class").value.sub("serendipity_imageComment_", "")
+        float = "class='figure-#{float}'"
+        style = imgcaption.attribute("style")
+        style = " style='#{style.value}'" if style
+        # Don't lose good data
+        mdbnum = imgcaption.search(".//comment()").text.strip.sub("s9ymdb:", "")
+        mdb = "<!-- mdb='#{mdbnum}' -->" if mdbnum
+        [float, style, mdb].join(" ")
       end
 
       def self.process_categories(db, options, post)
@@ -293,17 +491,35 @@ module JekyllImport
 
         db[cquery].each_with_object([]) do |tag, tags|
           tags << if options[:clean_entities]
-                    clean_entities(tag[:name])
+                    clean_entities(tag[:name]).downcase
                   else
-                    tag[:name]
+                    tag[:name].downcase
                   end
         end
       end
 
       def self.process_permalink(db, options, post)
-        return unless options[:permalinks]
+        return [] unless options[:permalinks]
+
+        permalinks = []
 
         px = options[:table_prefix]
+
+        if db.table_exists?("#{px}entryproperties")
+          pquery = %(
+            SELECT
+              props.value AS `permalink`
+            FROM
+              #{px}entryproperties AS props
+            WHERE
+              props.entryid = '#{post[:id]}' AND
+              props.property = 'permalink'
+          )
+          db[pquery].each do |link|
+            plink = link[:permalink].to_s
+            permalinks << plink unless plink.end_with? "/UNKNOWN.html"
+          end
+        end
 
         cquery = %(
             SELECT
@@ -316,8 +532,10 @@ module JekyllImport
         )
 
         db[cquery].each do |link|
-          return "/#{link[:permalink]}"
+          permalinks << "/#{link[:permalink]}"
         end
+
+        permalinks
       end
 
       def self.clean_entities(text)

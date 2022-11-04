405295af62d0d0067d6489a8ff31944b026e3aa2
sumanmaity112 <suman.maity112@gmail.com>
2022-11-04 22:56:07 +0100

    Use URI.open to open url and local file

--- lib/jekyll-import/importers/rss.rb
+++ lib/jekyll-import/importers/rss.rb
@@ -33,7 +33,7 @@ module JekyllImport
         source = options.fetch("source")
 
         content = ""
-        open(source) { |s| content = s.read }
+        URI.open(source) { |s| content = s.read }
         rss = ::RSS::Parser.parse(content, false)
 
         raise "There doesn't appear to be any RSS items at the source (#{source}) provided." unless rss

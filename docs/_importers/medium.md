75a158497daca01f2d8dc762380dedf23beffe12
sumanmaity112 <suman.maity112@gmail.com>
2022-11-29 19:38:45 +0100

    Accomodate PR feedbacks

--- docs/_importers/medium.md
+++ docs/_importers/medium.md
@@ -13,13 +13,15 @@ To import your posts from [Medium](https://medium.com/), run:
 $ ruby -r rubygems -e 'require "jekyll-import";
     JekyllImport::Importers::Medium.run({
         "username"       => "name",
-        "render_audio"   => "false",
-        "canonical_link" => "true",
+        "render_audio"   => false,
+        "canonical_link" => false,
     })'
 {% endhighlight %}
 
-The `username` refers the medium username and it's a mandatory field.
+The `username` refers to the medium username, and it's a mandatory field.
 
 Other optional fields are as follows:
-* `canonical_link` – copy original link as `canonical_url` to post. (default: `true`)
-* `render_audio` – render `<audio>` element in posts for the enclosure URLs (default: `false`)
+* `canonical_link` – copy original link as `canonical_url` to post. (default: `false`)
+* `render_audio` – render `<audio>` element in posts for the enclosure URLs. (default: `false`)
+
+_Note:_ This importer will also import the existing tags/labels from Medium post and include the tags to [Front Matter](https://jekyllrb.com/docs/front-matter/).
--- lib/jekyll-import/importers/medium.rb
+++ lib/jekyll-import/importers/medium.rb
@@ -4,9 +4,9 @@ module JekyllImport
   module Importers
     class Medium < Importer
       def self.specify_options(c)
-        c.option "username", "--username NAME", "Medium username"
-        c.option "canonical_link", "--canonical_link", "Copy original link as canonical_url to post. (default: false)"
-        c.option "render_audio", "--render_audio", "Render <audio> element in posts for the enclosure URLs (default: false)"
+        c.option "username",       "--username NAME",  "Medium username"
+        c.option "canonical_link", "--canonical_link", "Copy original link as canonical_url to post (default: false)"
+        c.option "render_audio",   "--render_audio",   "Render <audio> element in posts for the enclosure URLs (default: false)"
       end
 
       def self.validate(options)
@@ -14,16 +14,15 @@ module JekyllImport
       end
 
       def self.require_deps
-        JekyllImport.require_with_fallback(%w(
-          rss
-        ))
+        Importers::RSS.require_deps
       end
 
       def self.process(options)
-        RSS.process({
+        Importers::RSS.process({
           "source"         => "https://medium.com/feed/@#{options.fetch("username")}",
           "render_audio"   => options.fetch("render_audio", false),
-          "canonical_link" => options.fetch("canonical_link", true),
+          "canonical_link" => options.fetch("canonical_link", false),
+          # Add existing tags from Medium post to front matter using `category` subfield on the RSS <item>
           "extract_tags"   => "category",
         })
       end

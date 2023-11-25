844f59b7812db111ce841196bd35bd6d2bab0330
Ahmet ÖZER <10574489+ahmetozer@users.noreply.github.com>
2023-11-25 14:41:27 +0300

    Escape non ascii-characters

--- lib/jekyll-import/importers/wordpressdotcom.rb
+++ lib/jekyll-import/importers/wordpressdotcom.rb
@@ -28,7 +28,7 @@ module JekyllImport
 
         Jekyll.logger.info "Downloading images for ", title
         images.each do |i|
-          uri = i["src"]
+          uri = URI::Parser.new.escape(i["src"])
 
           dst = File.join(assets_folder, File.basename(uri))
           i["src"] = File.join("{{ site.baseurl }}", dst)
